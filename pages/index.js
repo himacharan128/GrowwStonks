6d5287b92411a90a9308286229a537b185cb9277
HIMA CHARAN GANGULA <himacharan128@gmail.com>
2023-10-30 06:35:01 +0530

    fix : import

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
 import temporaryData from "../utils/tempTopL&GData.json";
 import { fetchAndCacheData } from "./api/cache";
